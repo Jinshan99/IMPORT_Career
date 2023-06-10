594525cc8c59ce41a80c74f83e401da67a42f725
yangjins99 <yangjins@usc.edu>
2023-06-10 14:41:19 -0700

    first commit

---
+++ src/sections/resume/resume-card.js
@@ -0,0 +1,98 @@
+import PropTypes from 'prop-types';
+import ArrowDownOnSquareIcon from '@heroicons/react/24/solid/ArrowDownOnSquareIcon';
+import ClockIcon from '@heroicons/react/24/solid/ClockIcon';
+import { Avatar, Box, Card, CardContent, Divider, Stack, SvgIcon, Typography } from '@mui/material';
+
+export const ResumeCard = (props) => {
+    const { resume } = props;
+
+    return (
+        <Card
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                height: '100%'
+            }}
+        >
+            <CardContent>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        pb: 3
+                    }}
+                >
+                    <Avatar
+                        src={resume.logo}
+                        variant="square"
+                    />
+                </Box>
+                <Typography
+                    align="center"
+                    gutterBottom
+                    variant="h5"
+                >
+                    {resume.title}
+                </Typography>
+                <Typography
+                    align="center"
+                    variant="body1"
+                >
+                    {resume.description}
+                </Typography>
+            </CardContent>
+            <Box sx={{ flexGrow: 1 }} />
+            <Divider />
+            <Stack
+                alignItems="center"
+                direction="row"
+                justifyContent="space-between"
+                spacing={2}
+                sx={{ p: 2 }}
+            >
+                <Stack
+                    alignItems="center"
+                    direction="row"
+                    spacing={1}
+                >
+                    <SvgIcon
+                        color="action"
+                        fontSize="small"
+                    >
+                        <ClockIcon />
+                    </SvgIcon>
+                    <Typography
+                        color="text.secondary"
+                        display="inline"
+                        variant="body2"
+                    >
+                        Updated 2hr ago
+                    </Typography>
+                </Stack>
+                <Stack
+                    alignItems="center"
+                    direction="row"
+                    spacing={1}
+                >
+                    <SvgIcon
+                        color="action"
+                        fontSize="small"
+                    >
+                        <ArrowDownOnSquareIcon />
+                    </SvgIcon>
+                    <Typography
+                        color="text.secondary"
+                        display="inline"
+                        variant="body2"
+                    >
+                        {resume.downloads} Downloads
+                    </Typography>
+                </Stack>
+            </Stack>
+        </Card>
+    );
+};
+
+ResumeCard.propTypes = {
+    resume: PropTypes.object.isRequired
+};
